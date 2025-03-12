b13c47d245b2c667858e923291551a3ab0287d10
TGGamesYT <tothgergoci@gmail.com>
2025-03-12 18:52:46 +0100

    Update js.js

--- js.js
+++ js.js
@@ -2,12 +2,22 @@ function toggleMenu() {
     document.getElementById("sidebar").classList.toggle("active");
 }
 fetch('https://tggamesyt.github.io/outside.html')
-    .then(response => response.text())
-    .then(data => {
-        let temp = document.createElement('div');
-        temp.innerHTML = data;
-        let navContent = temp.querySelector('#navigation');
-        if (navContent) {
-            document.getElementById('navbar').innerHTML = navContent.innerHTML;
-        }
-    });
+.then(response => response.text())
+.then(data => {
+    let temp = document.createElement('div');
+    temp.innerHTML = data;
+    let navContent = temp.querySelector('#navigation');
+    if (navContent) {
+        document.getElementById('navbar').innerHTML = navContent.innerHTML;
+    }
+});
+fetch('https://tggamesyt.github.io/outside.html')
+.then(response => response.text())
+.then(data => {
+    let temp = document.createElement('div');
+    temp.innerHTML = data;
+    let navContent = temp.querySelector('#maintance');
+    if (navContent) {
+        document.getElementById('maintance').innerHTML = navContent.innerHTML;
+    }
+});
