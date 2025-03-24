0262107be3822c9a0e64537762227e89f199625a
TGGamesYT <tothgergoci@gmail.com>
2025-03-24 22:07:19 +0100

    Update server.sh

--- termuxmc/server.sh
+++ termuxmc/server.sh
@@ -171,6 +171,8 @@ resource-pack-sha1=
 spawn-protection=16
 max-world-size=29999984
 EOL
+# default server icon
+wget https://tggamesyt.github.io/termuxmc/termuxmc-icon.png -O icon.png
 
 # Create playitgg.sh
 echo "Downloading plugins.sh..."
